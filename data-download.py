7f17a99199ae04b3df3e5deb2b499c6d3663b8c8
arnavpadwal <arnav.padwal7@gmail.com>
2022-12-18 11:27:49 +0530

    bhav_save

--- data-download.py
+++ data-download.py
@@ -1,17 +1,10 @@
-from nsetools import Nse as ns
-from datetime import date, timedelta
-import requests
-import io
-import zipfile
-import jugaad_data.nse
-import time
+from datetime import date
 import pandas as pd
 
 # variables
 download_path = "//home//arnav//Work//VSCode//nse-data-organizer//daily_bhav"
 start_date = date(2022, 12, 1)
 end_date = date.today()
-holidays = []
 
 dates = pd.date_range(start_date, end_date)
 for i in dates:
@@ -19,20 +12,6 @@ for i in dates:
         jugaad_data.nse.bhavcopy_save(i, download_path)
     except Exception:
         continue
-
-
-# def daterange(start_date, end_date):
-#     for n in range(int((end_date - start_date).days)):
-#         yield start_date + timedelta(n)
-
-
-# for single_date in daterange(start_date, end_date):
-#     try:
-#         jugaad_data.nse.bhavcopy_save(single_date, download_path)
-#     except Exception:
-#         holidays.append(str(single_date))
-#         continue
-
 
 """
 url format: https://www1.nseindia.com/content/historical/EQUITIES/{DAY}/{MONTH}/cm{DAY}{MONTH}{YEAR}bhav.csv.zip
